9a741f9f7a186baccc387d1570db60bca6341c08
cskru <kruthika.csk@gmail.com>
2020-08-01 20:22:55 +0530

    Updates

---
+++ precache-manifest.45e5c70e7797f9d3bb79d62cbb25d551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba51085c67cb5f4121ac36b27f59ced3",
+    "url": "/flashclick/index.html"
+  },
+  {
+    "revision": "9fb08cc6f20331ca7357",
+    "url": "/flashclick/static/css/main.67ed2547.chunk.css"
+  },
+  {
+    "revision": "0e34e2ee6ad1484e3a65",
+    "url": "/flashclick/static/js/2.998038d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashclick/static/js/2.998038d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb08cc6f20331ca7357",
+    "url": "/flashclick/static/js/main.ceb0c3c3.chunk.js"
+  },
+  {
+    "revision": "13f70fb923d787cd9e98",
+    "url": "/flashclick/static/js/runtime-main.caf6b577.js"
+  }
+]);
